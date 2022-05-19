7bfcc102f5aaa6bae08faf21f8566f4ff8ec934b
egesaygili <eges96@gmail..com>
2022-05-19 20:18:24 +0200

    seems to be working still

---
+++ mpi/utilities/basic_utils.h
@@ -0,0 +1,9 @@
+double clamp(double val, double min, double max);
+
+void sleep_ms(double ms);
+
+double fRand(double fMin, double fMax);
+
+char* get_timestamp(char* timestamp);
+
+double get_distance(double x1, double x2, double y1, double y2);
---
+++ mpi/utilities/cjson_utils.h
@@ -0,0 +1,3 @@
+#include "cjson/cJSON.h"
+void create_json(cJSON* message, double x, double y, double value, double exceeded, char* timestamp);
+
